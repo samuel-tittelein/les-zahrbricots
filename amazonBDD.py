d0ee25ebee557b8e18e8d3b058d45dce54040526
Romain <r.hannoir@gmail.com>
2024-10-14 15:06:07 +0200

    #7

--- amazonBDD.py
+++ amazonBDD.py
@@ -1,35 +1,79 @@
 import sqlite3
 
-con = sqlite3.connect('amazonBDD.db')
+con = sqlite3.connect('dataBase.db')
 
 def createTables():
     cur = con.cursor()
-    with open("mon_script.sql") as fichier:
-        sql_script = fichier.read()
-        fichier.close()
-    cur.executescript(sql_script)
-    con.commit()
+    try:
+        with open("dataBaseAmazon.sql") as fichier:
+            sql_script = fichier.read()
+            fichier.close()
+        cur.executescript(sql_script)
+        con.commit()
+    except sqlite3.Error as e:
+        print(e)
+        return None
+    finally:
+        cur.close()
 
 def createUser(name, password):
     cur = con.cursor()
-    cur.execute("INSERT INTO user (name, password) VALUES ('?', '?')", [name, password])
-    con.commit()
-
+    try:
+        cur.execute("INSERT INTO user (name, password) VALUES ('?', '?')", [name, password])
+        con.commit()
+    except sqlite3.Error as e:
+        print(e)
+        return None
+    finally:
+        cur.close()
 
 def createProduct(name, price):
     cur = con.cursor()
-    cur.execute("INSERT INTO product (name, price) VALUES ('?', '?')", [name, price])
-    con.commit()
+    try:
+        cur.execute("INSERT INTO product (name, price) VALUES ('?', '?')", [name, price])
+        con.commit()
+    except sqlite3.Error as e:
+        print(e)
+        return None
+    finally:
+        cur.close()
+
 
 def createThePriceIsRight(id_user, id_product, nb_tries):
     cur = con.cursor()
-    cur.execute("INSERT INTO product (id_user, id_product, nb_tries) VALUES ('?', '?')", [id_user, id_product, nb_tries])
-    con.commit()
+    try:
+        cur.execute("INSERT INTO product (id_user, id_product, nb_tries) VALUES ('?', '?')", [id_user, id_product, nb_tries])
+        con.commit()
+    except sqlite3.Error as e:
+        print(e)
+        return None
+    finally:
+        cur.close()
 
-def getProductCategory(category):
+#renvoie tous les produits en fonction d'un tableau de categorie'
+def getProductsCategory(categories):
     cur = con.cursor()
-    cur.execute("SELECT * FROM ARTICLE WHERE id_category")
-    con.commit()
+    try:
+        cur.execute("SELECT * FROM ARTICLE WHERE id_category in ?", categories)
+        products = cur.fetchall()
+        return products
+    except sqlite3.Error as e:
+        print(e)
+        return None
+    finally:
+        cur.close()
 
+#renvoie tous les categories
+def getCategories():
+    cur = con.cursor()
+    try:
+        cur.execute("SELECT DISTINCT(category) FROM ARTICLE")
+        categories = cur.fetchall()
+        return categories
+    except sqlite3.Error as e:
+        print(e)
+        return None
+    finally:
+        cur.close()
 
 con.close()
