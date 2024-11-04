2ca231308711aae13c1da0decf887cfb785d5d37
Fierquin Matteo <matteo_fierquin@ens.univ-artois.fr>
2024-11-04 15:31:33 +0100

    Avancement sur la boucle principale #9

--- app.py
+++ app.py
@@ -1,46 +1,47 @@
 from flask import Flask, render_template, request
-import amazonBDD
 import api
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('acceuil.html', categories=["Informatique","Musique","Vetements"]) #A modifier
+    return render_template('acceuil.html', categories=["Informatique", "Musique", "Vetements"])
 
 @app.route('/game', methods=['POST'])
 def game():
-    #name = request.form['username']
-    #amazonBDD.createUser(name,password)
-    set_item()
-    return render_template('index.html')
+    set_item()  # Set initial product data before rendering
+    return render_template('index.html', name=product_name, price=product_price)
 
-price = 15 #prix du produit /!\ a recuperer de la base de donnees
-name = "test" #nom du produit /!\ a recuperer de la base de donnees
+product_price = 15  # default price, to be updated from the API
+product_name = "test"  # default name, to be updated from the API
 
 def set_item():
-    global name,price
-    id_product = api.get_random_id_product("Informatique")
-    name, price, img = api.get_infos(id_product)
-
-
+    global product_name, product_price
+    category = request.form.get('categorie')
+    #id = api.get_random_id_product(category)
+    id = "B076125HMB"
+    product_name, product_price, img = api.get_infos(id)
+    print(product_name, product_price)
 
 @app.route('/guess', methods=['POST'])
 def guess():
-    guess_price = request.form['guess']
-    if guess_price == '':
-        return render_template('index.html', response='Merci d\'entrer un prix',name=name)
-    else:
-        guess_price = float(guess_price)
-        #A modifier ça fait trop de return
-        if guess_price < price:
-            return render_template('index.html', response="Vous avez entrez " + str(guess_price) + "$ ce qui est trop bas",name=name)
-        elif guess_price > price:
-            return render_template('index.html', response="Vous avez entrez " + str(guess_price) + "$ ce qui est trop haut",name=name)
-        else:
-            #Le prix est bon faut changer l'item
-            return render_template('index.html', response="You guessed the correct price",name=name0)
+    global product_name, product_price
+    guess_price = request.form.get('guess', '').strip()
 
+    if not guess_price:
+        return render_template('index.html', response='Merci d\'entrer un prix', name=product_name, price=product_price)
+    
+    guess_price = float(guess_price)
+
+    if guess_price < product_price:
+        response = f"Vous avez entrez {guess_price}€, ce qui est trop bas"
+    elif guess_price > product_price:
+        response = f"Vous avez entrez {guess_price}€, ce qui est trop haut"
+    else:
+        response = "You guessed the correct price"
+        set_item()  # Reset item on correct guess
+
+    return render_template('index.html', response=response, name=product_name, price=product_price)
 
 if __name__ == '__main__':
     app.run(debug=True)
