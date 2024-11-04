f1e63edb796dd6af6a5ecd72f9a381bb35f4bed2
Tittelein Samuel <samuel_tittelein@ens.univ-artois.fr>
2024-11-04 14:32:54 +0100

    :construction: debug2

--- api.py
+++ api.py
@@ -2,9 +2,11 @@ import requests
 import random
 import sqlite3
 
+from amazonBDD import getProductsCategory
+
 def get_random_id_product(categorie):
     con = sqlite3.connect('amazonBDD.db')
-    products = getProductCategory(categorie)
+    products = getProductsCategory(categorie)
     con.close()
     return random.choice(products)
 
