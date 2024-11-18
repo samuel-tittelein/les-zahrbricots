bbbd45ccd1cdb146e2e4eba5e571c29d97a2873b
Tittelein Samuel <samuel_tittelein@ens.univ-artois.fr>
2024-11-18 13:14:57 +0100

    #17 ajout d'une fonction pour verifier la validité du produit

--- fill_db.py
+++ fill_db.py
@@ -1,3 +1,6 @@
+from argparse import ArgumentError
+
+import requests
 from unicodedata import category
 
 from amazonBDD import *
@@ -12,5 +15,21 @@ def fill_product(file_path):
 
     for product in values:
         id_product = product[0]
+        if check_id(id_product):
+            continue
         category = product[1]
         createProduct(id_product, category)
+
+def check_id(id_product):
+    url = "http://ws.chez-wam.info/" + id_product
+    try :
+        response = requests.get(url).json();
+        name = response['title'].split(',')[0]
+        price = response['price']
+        img = response['images'][0]
+    except :
+        return False
+    if name is None or price is None or img is None:
+        return False
+    return True
+
